36e9e7ca42fa69e71b7b98a3cc195ead4ec0ee3d
Sagar <sagargaikwad3851@gmail.com>
2022-11-08 09:54:46 +0530

    App Launch From activity_main.xml, This is a splash Screen named MainSplash.java.

---
+++ app/src/main/java/com/example/homeworkout/splashscreen/MainSplash.java
@@ -0,0 +1,16 @@
+package com.example.homeworkout.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homeworkout.R;
+
+public class MainSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
